5db6c31c08368ecf1c52fb63642d3a36e7780851
Abdulrahman I. Mahmoud <126473936+abdoibrahim1337@users.noreply.github.com>
2024-06-19 14:12:11 +0300

    Create HuffmanNode.java

---
+++ HuffmanNode.java
@@ -0,0 +1,14 @@
+public class HuffmanNode implements Comparable<HuffmanNode> {
+    char data;
+    int frequency;
+    HuffmanNode left, right;
+
+    public HuffmanNode(char data, int frequency) {
+        this.data = data;
+        this.frequency = frequency;
+    }
+
+    public int compareTo(HuffmanNode node) {
+        return this.frequency - node.frequency;
+    }
+}
